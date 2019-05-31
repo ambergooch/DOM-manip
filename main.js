acce19c1898862fc23cd1c93f935873b6e2f856b
Amber Gooch <agooch91@gmail.com>
2019-05-31 13:28:31 -0500

    Added HTML, CSS, and JS files

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector(".article__header").textContent = "Welcome to Amber's blog";
+
+document.querySelectorAll(".article__header").classList = "article__header important";
+
+document.querySelector(".dashed").classList.remove("dashed");
+
+document.querySelector(".article__footer").classList.add("goldenrod");
